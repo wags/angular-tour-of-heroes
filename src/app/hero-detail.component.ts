447666c0c8830da17415d34aa69e108ecfc3eae0
Matt Wagner <matt@wags.us>
2017-06-25 19:09:56 -0400

    Add hero detail styling

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,7 +8,8 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
