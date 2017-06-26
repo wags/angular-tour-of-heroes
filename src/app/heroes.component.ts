8c7bfe235bb48631f6b97bb4233dc87c38eab2be
Matt Wagner <matt@wags.us>
2017-06-26 12:45:58 -0400

    Update heroes service to use HTTP

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -23,8 +23,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    // this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
