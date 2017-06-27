9083ae2f691cebebeed4bb0200dc47f6d66b10a0
Matt Wagner <matt@wags.us>
2017-06-27 12:48:47 -0400

    Add ability to update hero details

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,7 @@ import { Hero } from './hero';
 export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -36,6 +37,15 @@ export class HeroService {
     return this.http.get(url)
       .toPromise()
       .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
       .catch(this.handleError);
   }
 }
